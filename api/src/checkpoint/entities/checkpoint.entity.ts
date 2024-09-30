c30069041d9fdbe8acd63e9f8f5c813f83bea213
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-09-30 11:13:54 -0600

    Se creó la entidad Checkpoint

--- api/src/checkpoint/entities/checkpoint.entity.ts
+++ api/src/checkpoint/entities/checkpoint.entity.ts
@@ -1,1 +1,14 @@
-export class Checkpoint {}
+import { User } from "src/user/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Checkpoint {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column({type: 'point'})
+    coords: string;
+    @ManyToOne(() => User, user => user.checkpoints)
+    user: User;
+}
