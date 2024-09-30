98ac2ae1d9446623c3598b31f9e154e0aba75a0d
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-09-30 11:52:59 -0600

    se creó el DTO para guardar un checkpoint

--- api/src/checkpoint/dto/create-checkpoint.dto.ts
+++ api/src/checkpoint/dto/create-checkpoint.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCheckpointDto {}
+import { IsDecimal, IsString } from "class-validator";
+
+export class CreateCheckpointDto {
+    @IsString()
+    name: string;
+    @IsDecimal()
+    latitude: number;
+    @IsDecimal()
+    longitude: number;
+    @IsString()
+    userId: string;
+}
