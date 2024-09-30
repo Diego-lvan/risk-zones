e9f975d1ccda538667f0ef922442bec9a8cab95f
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-09-30 09:48:51 -0600

    Se incorpora el modulo para obtener la lista de puntos de riesgo y se importa la configuración para la base de datos

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConstants } from './constants/db.constants';
+import { User } from './user/entities/user.entity';
+import { News } from './risk-zones/entities/news.entity';
+import { RiskAreasModule } from './risk-zones/risk-areas.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: DbConstants.DB_HOST,
+      port: DbConstants.DB_PORT,
+      username: DbConstants.DB_USER,
+      password: DbConstants.DB_PASSWORD,
+      database: DbConstants.DB_NAME,
+      entities: [
+        User,
+        News
+      ],
+      synchronize: true,
+      logging: false,
+    }),
+    RiskAreasModule
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [TypeOrmModule]
 })
 export class AppModule {}
