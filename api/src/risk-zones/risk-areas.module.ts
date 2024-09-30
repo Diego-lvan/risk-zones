18fe98cd072183054a778b21ecf53f6e08c710c5
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-09-30 09:47:55 -0600

    Se crea el modulo para obtener la lista de zonas de riesgo

---
+++ api/src/risk-zones/risk-areas.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { RiskAreasController } from "./risk-areas.controller";
+import { RiskAreasService } from "./riskareas.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { News } from "./entities/news.entity";
+import { User } from "src/user/entities/user.entity";
+
+@Module({
+  controllers: [RiskAreasController],
+  providers: [
+    RiskAreasService,
+  ],
+  imports: [
+    TypeOrmModule.forFeature([News, User])
+  ],
+})
+export class RiskAreasModule {}
