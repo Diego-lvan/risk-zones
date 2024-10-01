7d68255098a986dc86951ee9f21a76723f3e66fe
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-10-01 17:47:21 -0600

    agregando endpoint para mandar una notificacion

--- api/src/checkpoint/checkpoint.controller.ts
+++ api/src/checkpoint/checkpoint.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CheckpointService } from './checkpoint.service';
 import { CreateCheckpointDto } from './dto/create-checkpoint.dto';
-import { UpdateCheckpointDto } from './dto/update-checkpoint.dto';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { SendNotificationReqDto } from './dto/send-notification-req-dto';
 
 @Controller('checkpoint')
+@ApiTags('checkpoint')
 export class CheckpointController {
   constructor(private readonly checkpointService: CheckpointService) {}
 
@@ -12,23 +14,20 @@ export class CheckpointController {
     return this.checkpointService.create(createCheckpointDto);
   }
 
+  @Post('notify')
+  @ApiBody({ type: SendNotificationReqDto })
+  async notifyCheckpointPassed(@Body() sendNotificationReqDto: SendNotificationReqDto) {
+    try {
+      await this.checkpointService.notifyCheckpointPassed(sendNotificationReqDto);
+    } catch (e) {
+      return e;
+    }
+  }
+
   @Get('user/:id')
   findAll(@Param('id') id: string) {
     return this.checkpointService.findAllByUser(id);
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.checkpointService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCheckpointDto: UpdateCheckpointDto) {
-    return this.checkpointService.update(+id, updateCheckpointDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.checkpointService.remove(+id);
-  }
 }
+
+// 3e36bd07-4714-4527-8c43-2d17144e5914
