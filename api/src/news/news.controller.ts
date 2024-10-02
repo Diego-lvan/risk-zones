d789baf77ed9d6994d3e31a48b2ae8fe239b7704
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-01 18:41:40 -0600

    Se creó NewsController

---
+++ api/src/news/news.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { NewsService } from './news.service';
+import { CreateNewsDto } from './dto/create-news.dto';
+import { UpdateNewsDto } from './dto/update-news.dto';
+import { News } from './entities/news.entity';
+
+@Controller('news')
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Post()
+  create(@Body() createNewsDto: CreateNewsDto): Promise<News> {
+    return this.newsService.createNews(createNewsDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.newsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.newsService.findOne(+id);
+  }
+
+}
