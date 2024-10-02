f43d0ba3385a1d76c79fd7f89172fb54834f1667
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-10-02 15:29:18 -0600

    Añadiendo documentación de swagger

--- api/src/checkpoint/checkpoint.controller.ts
+++ api/src/checkpoint/checkpoint.controller.ts
@@ -1,34 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CheckpointService } from './checkpoint.service';
 import { CreateCheckpointDto } from './dto/create-checkpoint.dto';
-import { UpdateCheckpointDto } from './dto/update-checkpoint.dto';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 @Controller('checkpoint')
+@ApiTags('Checkpoint')
 export class CheckpointController {
   constructor(private readonly checkpointService: CheckpointService) {}
 
   @Post()
+  @ApiBody({ type: CreateCheckpointDto })
   create(@Body() createCheckpointDto: CreateCheckpointDto) {
     return this.checkpointService.create(createCheckpointDto);
-  }
-
-  @Get('user/:id')
-  findAll(@Param('id') id: string) {
-    return this.checkpointService.findAllByUser(id);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.checkpointService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCheckpointDto: UpdateCheckpointDto) {
-    return this.checkpointService.update(+id, updateCheckpointDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.checkpointService.remove(+id);
   }
 }
--- api/src/checkpoint/dto/create-checkpoint.dto.ts
+++ api/src/checkpoint/dto/create-checkpoint.dto.ts
@@ -1,16 +1,24 @@
-import { IsDecimal, IsNumber, IsString, Max, Min } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class CreateCheckpointDto {
-    @IsString()
-    name: string;
-    @IsNumber()
-    @Max(90)
-    @Min(-90)
-    latitude: number;
-    @IsNumber()
-    @Max(180)
-    @Min(-180)
-    longitude: number;
-    @IsString()
-    userId: string;
+  @ApiProperty({ description: 'Name of the checkpoint', example: 'Checkpoint 1' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'Latitude of the checkpoint', example: 40.416775 })
+  @IsNumber()
+  @Max(90)
+  @Min(-90)
+  latitude: number;
+
+  @ApiProperty({ description: 'Longitude of the checkpoint', example: -3.70379 })
+  @IsNumber()
+  @Max(180)
+  @Min(-180)
+  longitude: number;
+
+  @ApiProperty({ description: 'User id that made the checkpoint' })
+  @IsString()
+  userId: string;
 }
