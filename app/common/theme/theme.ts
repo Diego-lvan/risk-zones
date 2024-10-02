92eb2b325810d6b4521cff3cd9bd0c3c8de34948
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-10-02 17:08:11 -0600

    Se creó el objeto theme para estandarizar los colores de la aplicación

---
+++ app/common/theme/theme.ts
@@ -0,0 +1,19 @@
+interface Theme {
+  colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    background: string;
+    text: string;
+  };
+}
+
+const APP_THEME: Theme = {
+  colors: {
+    primary: "#007bff",
+    secondary: "#6c757d",
+    tertiary: "#6c757d",
+    background: "#f8f9fa",
+    text: "#343a40",
+  },
+};
