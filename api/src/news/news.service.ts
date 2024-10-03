a33c92299aac95db0ad0acefcf22e99e88a49f3c
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-03 13:09:29 -0600

    Se agrega metodo para contar palabras y validar tanto el titulo como el contenido

--- api/src/news/news.service.ts
+++ api/src/news/news.service.ts
@@ -14,6 +14,14 @@ export class NewsService {
   ) {}
 
   async createNews(createNewsDto: CreateNewsDto): Promise<News> {
+    // Validar que el titulo no exceda de 6 palabras
+    if (this.countWords(createNewsDto.title) > 6) {
+      throw new Error('Title exceeds 6 words');
+    }
+    // Validar que el contenido no exceda de 50 palabras
+    if (this.countWords(createNewsDto.content) > 50) {
+      throw new Error('Content exceeds 50 words');
+    }
     const user = await this.userService.findOne(createNewsDto.user);
     if (!user) {
       throw new Error('User not found');
@@ -37,6 +45,10 @@ export class NewsService {
 
     // Guardar la noticia en la base de datos
     return this.newsRepository.save(news);
+  }
+  // Método para contar las palabras
+  private countWords(text: string): number {
+    return text.split(' ').filter((word) => word.length > 0).length;
   }
 
   create() {
