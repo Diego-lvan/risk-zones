bdd997f17a9f79792a500d60a54cb5d579d0bebd
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-10-03 13:14:21 -0600

    actualizando codigo de status para CheckPointNotFound error

--- api/src/checkpoint/errors/checkpoint-not-found.error.ts
+++ api/src/checkpoint/errors/checkpoint-not-found.error.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class CheckPointNotFound extends HttpException {
   constructor() {
-    super('The checkpoint does not exist', HttpStatus.BAD_REQUEST);
+    super('The checkpoint does not exist', HttpStatus.NOT_FOUND);
   }
 }
