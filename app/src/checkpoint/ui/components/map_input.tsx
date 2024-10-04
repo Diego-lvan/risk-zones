bb0af87f28994af9e951f677d26e535ecf5bafc8
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-10-04 11:41:09 -0600

    Se agregó el marcador para ver donde esta el checkpoint selecionado

--- app/src/checkpoint/ui/components/map_input.tsx
+++ app/src/checkpoint/ui/components/map_input.tsx
@@ -1,22 +1,49 @@
 import { APP_THEME } from "@/common/theme/theme";
+import { useSelectLocation } from "@/src/common/context/location_context";
+import { router, Href } from "expo-router";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 interface MapInputProps {
   label?: string;
+  coordinates?: { latitude: number; longitude: number };
+  handleChangedCoordinates?: (coordinates: {
+    latitude: number;
+    longitude: number;
+  }) => void;
+  error?: string;
 }
 
-export const MapInput = ({ label }: MapInputProps) => {
+export const MapInput = ({ label, error }: MapInputProps) => {
+  const { location: coordinates } = useSelectLocation();
   const handlePress = () => {
-    console.log("Map pressed");
+    router.push("./select_map_location");
   };
-
   return (
     <View style={styles.mainContainer}>
       {label && <Text style={styles.label}>{label}</Text>}
       <TouchableOpacity style={styles.map} onPress={handlePress}>
-        <MapView style={{ flex: 1 }} scrollEnabled={false} />
+        <MapView
+          style={{ flex: 1 }}
+          scrollEnabled={false}
+          region={{
+            latitude: coordinates?.latitude || 0,
+            longitude: coordinates?.longitude || 0,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          {coordinates && (
+            <Marker
+              coordinate={{
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude,
+              }}
+            />
+          )}
+        </MapView>
       </TouchableOpacity>
+      {error && <Text style={{ color: "red" }}>{error}</Text>}
     </View>
   );
 };
