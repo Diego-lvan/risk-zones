cae359966ab68f156c82044c293ffeacb4a0c016
Diego Iván <80365304+Diego-lvan@users.noreply.github.com>
2024-10-04 14:48:37 -0600

    agregando modal al checkpoint screen

--- app/src/checkpoint/ui/screens/checkpoint_screen.tsx
+++ app/src/checkpoint/ui/screens/checkpoint_screen.tsx
@@ -1,12 +1,30 @@
 import { View, StyleSheet } from "react-native";
 import { AddCheckpointButton } from "../components/add_checkpoint_button";
 import { StartRouteButton } from "../components/start_route_button";
+import { useState } from "react";
+import { ContactFormModal } from "../components/contact_form_modal";
 
 export const CheckpointScreen = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isActiveRoute, setIsActiveRoute] = useState(false);
+
+  const handlePressStartRoute = () => {
+    // logic when the route is started
+    if (!isActiveRoute) {
+      setIsModalVisible(true);
+      return;
+    }
+    // logic when the route is finished
+    setIsActiveRoute(false);
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <StartRouteButton />
+      <StartRouteButton handleOnPress={handlePressStartRoute} isActiveRoute={isActiveRoute} />
       <AddCheckpointButton />
+
+      {/* Mostrar el modal dependiendo de la visibilidad */}
+      <ContactFormModal showModal={isModalVisible} setIsModalVisible={setIsModalVisible} setIsActiveRoute={setIsActiveRoute} />
     </View>
   );
 };
