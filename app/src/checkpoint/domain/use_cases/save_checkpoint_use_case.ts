865c69868dce3d9af5b110a60ae23637b0aff2cb
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-10-05 20:48:48 -0600

    Se creó el caso de uso para guardar un checkpoint

--- app/src/checkpoint/domain/use_cases/save_checkpoint_use_case.ts
+++ app/src/checkpoint/domain/use_cases/save_checkpoint_use_case.ts
@@ -1,18 +1,25 @@
+import { ApiError } from "@/src/common/errors/api_error";
 import { CheckpointForm } from "../../hooks/useSaveCheckpoint";
+import { CheckpointDataSourceImpl } from "../../infrastructure/datasources/checkpoint_datasource";
+import { CheckpointRepositoryImpl } from "../../infrastructure/repositories/checkpoint_repository";
 import { CheckpointEntity } from "../entities/checkpoint_entity";
+import { CheckpointRepository } from "../repositories/checkpoint_repository";
 
 export class SaveCheckpointUseCase {
-  //private checkpointRepository: CheckpointRepository;
+  private checkpointRepository: CheckpointRepository;
   constructor() {
-    //this.checkpointRepository = new CheckpointRepository();
+    const datasource = new CheckpointDataSourceImpl();
+    this.checkpointRepository = new CheckpointRepositoryImpl(datasource);
   }
   public async execute(checkpoint: CheckpointForm) {
     try {
       const checkpointEntity = this.validate(checkpoint);
-      //await this.checkpointRepository.saveCheckpoint(checkpoint);
+      await this.checkpointRepository.saveCheckpoint(checkpointEntity);
     } catch (error) {
-      console.log(error);
-      throw new Error("Error saving checkpoint");
+      if (error instanceof ApiError) {
+        throw error;
+      }
+      throw new ApiError("Error saving checkpoint", 500);
     }
   }
 
@@ -35,12 +42,17 @@ export class SaveCheckpointUseCase {
       throw new Error("Invalid longitude");
     }
 
+    if (!checkpoint.userId) {
+      throw new Error("Invalid user");
+    }
+
     return {
       name: checkpoint.name,
       coords: {
         latitude: checkpoint.latitude,
         longitude: checkpoint.longitude,
       },
+      userId: checkpoint.userId,
     };
   }
 }
