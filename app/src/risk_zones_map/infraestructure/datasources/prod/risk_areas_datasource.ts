9f5dad29bd49b52c07d4fded44bd37ed775be73e
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-10-07 07:59:02 -0600

    Se formatea el codigo

--- app/src/risk_zones_map/infraestructure/datasources/prod/risk_areas_datasource.ts
+++ app/src/risk_zones_map/infraestructure/datasources/prod/risk_areas_datasource.ts
@@ -4,20 +4,27 @@ import { RiskPoint } from "@/src/risk_zones_map/domain/entities/risk_point_entit
 import axios from "axios";
 
 export class RiskAreasDatasourceImplProd implements RiskAreasDatasource {
-    async getRiskPoints(latitude: number, longitude: number, radius: number): Promise<RiskPoint[]> {
-        const {data, status} = await axios.get<RiskPoint[]>(API_URL + '/risk-areas', {
-            params: {
-              longitude: longitude,
-              latitude: latitude,
-              radius: radius
-            },
-            timeout: 5000
-        });
+  async getRiskPoints(
+    latitude: number,
+    longitude: number,
+    radius: number
+  ): Promise<RiskPoint[]> {
+    const { data, status } = await axios.get<RiskPoint[]>(
+      API_URL + "/risk-areas",
+      {
+        params: {
+          longitude: longitude,
+          latitude: latitude,
+          radius: radius,
+        },
+        timeout: 5000,
+      }
+    );
 
-        if( status != 200){
-            throw new Error('Error fetching risk points');
-        }
-
-        return data;
+    if (status != 200) {
+      throw new Error("Error fetching risk points");
     }
+
+    return data;
+  }
 }
