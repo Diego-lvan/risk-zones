82bb5dc75fcbe81b6a3b2f3c23967b609db923cd
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-07 19:26:40 -0600

    se actualizo el save news error

--- app/app/save_news_error.tsx
+++ app/app/save_news_error.tsx
@@ -1,21 +1,28 @@
-import { SaveNewsErrorScreen } from "@/src/news/ui/screens/error_saving_news_screen";
+import { SaveNewsConfirmation } from "@/src/news/ui/screens/error_saving_news_screen";
 import { UnknownOutputParams, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 
-export default function SaveCheckpointError() {
+export default function SaveNewsError() {
   const { known_error, message } = useLocalSearchParams<{
     known_error?: string;
     message?: string;
   }>();
 
+  const [isModalVisible, setModalVisible] = useState(true);
+
   const isKnownError = known_error ? known_error === "true" : false;
   const errorMessage = message
     ? message.split("_").join(" ")
-    : "Error al guardar el checkpoint, intenta más tarde";
+    : "Error al guardar la noticia, intenta más tarde";
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
 
   return (
-    <SaveNewsErrorScreen
-      isKnownError={isKnownError}
-      errorMessage={errorMessage}
+    <SaveNewsConfirmation
+      isVisible={isModalVisible}
+      onClose={handleCloseModal}
     />
   );
 }
