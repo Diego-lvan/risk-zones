0b601c6a22a65ae0114a83b2a1d589c2bbead19f
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-24 20:33:57 -0600

    Se agrega dto para crear un reporte de iluminación

--- api/src/lighting-report/dto/create-lighting-report.dto.ts
+++ api/src/lighting-report/dto/create-lighting-report.dto.ts
@@ -1,1 +1,33 @@
-export class CreateLightingReportDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { User } from 'src/user/entities/user.entity';
+import { Point } from 'typeorm';
+
+export class CreateLightingReportDto {
+  @ApiProperty({ description: 'Título del reporte de iluminación' })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({ description: 'Contenido del reporte de iluminación' })
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @ApiProperty({ description: 'Punto de inicio del reporte de iluminación' })
+  @ValidateNested()
+  startPoint: Point;
+
+  @ApiProperty({ description: 'Punto final del reporte de iluminación' })
+  @ValidateNested()
+  endPoint: Point;
+
+  @ApiProperty({ description: 'Fecha de publicación en formato personalizado', example: 'DD/MM/YYYY' })
+  @IsNotEmpty()
+  date: Date;
+
+  @ApiProperty({ description: 'User id that made the lighting report' })
+  @IsString()
+  @IsNotEmpty()
+  user: User;
+}
