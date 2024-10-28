90f5fac8dd1b8be65e2b92fda446138bb92253ac
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-10-27 18:00:22 -0600

    Se implementó el método para obtener la información de una noticia desde el datasource

--- app/src/news/infrastructure/repositories/news_repository.ts
+++ app/src/news/infrastructure/repositories/news_repository.ts
@@ -1,5 +1,6 @@
 import { NewsDataSource } from "../../domain/datasources/news_datasource";
 import { NewsEntity } from "../../domain/entities/news_entity";
+import { NewInfoEntity } from "../../domain/entities/see_new_entity";
 import { NewsRepository } from "../../domain/repositories/news_repository";
 
 export class NewsRepositoryImpl implements NewsRepository {
@@ -7,6 +8,9 @@ export class NewsRepositoryImpl implements NewsRepository {
 
   constructor(dataSource: NewsDataSource) {
     this.dataSource = dataSource;
+  }
+  async getNewInfo(newId: number): Promise<NewInfoEntity> {
+    return await this.dataSource.getNewInfo(newId);
   }
   async saveNews(news: NewsEntity): Promise<void> {
     return await this.dataSource.saveNews(news);
