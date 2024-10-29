40f192b052d41293e94d07bb39dd2a1f4651e8c1
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-29 16:04:37 -0600

    Se crea select map points location

---
+++ app/app/select_map_points_location.tsx
@@ -0,0 +1,5 @@
+import { SelectMapPointsScreen } from "../src/lighting_report/ui/screens/select-map-points-screen";
+
+export default function SelectMapPointsLocation() {
+  return <SelectMapPointsScreen />;
+}
