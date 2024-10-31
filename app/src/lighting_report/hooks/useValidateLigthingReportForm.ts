e24d30d7ff0d32227fb7ffbfce7f0f80337d5671
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-10-31 10:21:31 -0600

    Update useValidateLigthingReportForm.ts

--- app/src/lighting_report/hooks/useValidateLigthingReportForm.ts
+++ app/src/lighting_report/hooks/useValidateLigthingReportForm.ts
@@ -1,25 +1,37 @@
-import { useSelectLocationTwoPoints } from "@/src/common/context/location_context";
+import { useSelectLocationTwoPoints } from "@/src/common/context/location_two_points_context";
 import { useUser } from "@/src/user/context/user_context";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 
 const lighting_report_schema = Yup.object().shape({
-  date: Yup.date().required("La fecha es requerida"),
+  created_at: Yup.date().required("La fecha es requerida"),
   userId: Yup.string().required("El usuario es requerido"),
-  startCoords: Yup.mixed().required("Las coordenadas de inicio son requeridas"),
-  endCoords: Yup.mixed().required("Las coordenadas de fin son requeridas"),
-  latitude: Yup.number().required("La latitud es requerida"),
-  longitude: Yup.number().required("La longitud es requerida"),
+  startLatitude: Yup.number()
+    .required("Start Latitude is required")
+    .min(-90, "Invalid latitude")
+    .max(90, "Invalid latitude"),
+  startLongitude: Yup.number()
+    .required("Start Longitude is required")
+    .min(-180, "Invalid longitude")
+    .max(180, "Invalid longitude"),
+  endLatitude: Yup.number()
+    .required("End Latitude is required")
+    .min(-90, "Invalid latitude")
+    .max(90, "Invalid latitude"),
+  endLongitude: Yup.number()
+    .required("End Longitude is required")
+    .min(-180, "Invalid longitude")
+    .max(180, "Invalid longitude"),
 });
 
 export interface LightingReportForm {
-  endCoords: any;
-  startCoords: any;
+  startLatitude: number;
+  startLongitude: number;
+  endLatitude: number;
+  endLongitude: number;
   userId: string;
-  date: Date;
-  latitude: number;
-  longitude: number;
+  created_at: Date;
 }
 
 type LightingReportFormFields = keyof LightingReportForm;
@@ -43,25 +55,42 @@ export const useValidatedForm = ({
 
   const { user } = useUser();
   const { startCoords, endCoords } = useSelectLocationTwoPoints();
-
+  console.log("Start Coords:", startCoords);
+  console.log("End Coords:", endCoords);
   const updateValue = (
     name: LightingReportFormFields,
-    value: string | number | Date
+    value: string | Date | { latitude: number; longitude: number }
   ) => {
-    setValue(name, value);
+    if (
+      typeof value === "object" &&
+      "latitude" in value &&
+      "longitude" in value
+    ) {
+      if (name.includes("start")) {
+        setValue("startLatitude", value.latitude);
+        setValue("startLongitude", value.longitude);
+      } else if (name.includes("end")) {
+        setValue("endLatitude", value.latitude);
+        setValue("endLongitude", value.longitude);
+      }
+    } else {
+      setValue(name, value);
+    }
     trigger(name);
   };
 
   const onSubmit = async () => {
+    console.log("Start Coords:", startCoords);
+    console.log("End Coords:", endCoords);
     if (startCoords && endCoords) {
-      updateValue("latitude", startCoords.latitude);
-      updateValue("longitude", startCoords.longitude);
-      updateValue("latitude", endCoords.latitude);
-      updateValue("longitude", endCoords.longitude);
+      setValue("startLatitude", startCoords.latitude);
+      setValue("startLongitude", startCoords.longitude);
+      setValue("endLatitude", endCoords.latitude);
+      setValue("endLongitude", endCoords.longitude);
     }
     updateValue("userId", user?.id || "");
     console.log(user?.id);
-    updateValue("date", new Date());
+    updateValue("created_at", new Date());
 
     await handleSubmit(
       async (data) => {
