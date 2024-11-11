37a25fd7941b2d425425859d33ccad4757125c33
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-11-11 13:56:19 -0600

    Se agrega la entidad LightingRating al modulo de calles iluminadas

--- api/src/lighted-streets/lighted-streets.module.ts
+++ api/src/lighted-streets/lighted-streets.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LightedStreet } from './entities/lighted_street.entity';
 import { User } from 'src/user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
+import { LightingRating } from './entities/lighting_rating.entity';
 
 @Module({
   controllers: [LightedStreetsController],
   providers: [LightedStreetsService],
-  imports: [TypeOrmModule.forFeature([LightedStreet, User]), UserModule],
+  imports: [TypeOrmModule.forFeature([LightedStreet, User, LightingRating]), UserModule],
 })
 export class LightedStreetModule {}
