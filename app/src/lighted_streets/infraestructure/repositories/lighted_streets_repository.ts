3f3ac9ef5cfdc6521a0091db4919c96c4c9650c7
Omar Luna Hernández <42101656@uaz.edu.mx>
2024-11-13 12:49:57 -0600

    Se implementa el metodo de calificar una calle iluminada en la clase del repositorio

--- app/src/lighted_streets/infraestructure/repositories/lighted_streets_repository.ts
+++ app/src/lighted_streets/infraestructure/repositories/lighted_streets_repository.ts
@@ -1,10 +1,26 @@
 import { LightedStreetsDatasource } from "../../domain/datasources/lighted_streets_datasource";
 import { LightedStreet } from "../../domain/entities/lighted_street";
+import { LightedStreetRatingForm } from "../../domain/entities/lighted_street_rating_form";
 import { LightedStreetsRepository } from "../../domain/repositories/lighted_streets_repository";
 
+/**
+ * Implementation of the LightedStreetsRepository interface.
+ * This class is responsible for interacting with the data source to fetch and rate lighted streets.
+ */
 export class LightedStreetsRepositoryImpl implements LightedStreetsRepository {
+  /**
+   * Constructor for LightedStreetsRepositoryImpl.
+   * @param lightedStreetsDatasource - The data source to interact with for lighted streets data.
+   */
   constructor(private lightedStreetsDatasource: LightedStreetsDatasource) {}
 
+  /**
+   * Fetches lighted streets within a specified radius from a given latitude and longitude.
+   * @param latitude - The latitude to search from.
+   * @param longitude - The longitude to search from.
+   * @param radius - The radius within which to search for lighted streets.
+   * @returns A promise that resolves to an array of LightedStreet objects.
+   */
   async getLightedStreets(
     latitude: number,
     longitude: number,
@@ -15,5 +31,14 @@ export class LightedStreetsRepositoryImpl implements LightedStreetsRepository {
       longitude,
       radius
     );
+  }
+
+  /**
+   * Submits a rating for a lighted street.
+   * @param form - The form containing the rating details.
+   * @returns A promise that resolves when the rating has been submitted.
+   */
+  async rateLightedStreet(form: LightedStreetRatingForm): Promise<void> {
+    return this.lightedStreetsDatasource.rateLightedStreet(form);
   }
 }
