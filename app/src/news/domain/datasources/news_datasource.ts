692f5e7d9d8703bb1bdba60479ebba281fa5a1f6
Giovanna Esmeralda Chavez Espino <79319097+gio-gigi@users.noreply.github.com>
2024-11-19 11:27:08 -0600

    Update news_datasource.ts

--- app/src/news/domain/datasources/news_datasource.ts
+++ app/src/news/domain/datasources/news_datasource.ts
@@ -1,7 +1,9 @@
 import { NewsEntity } from "../entities/news_entity";
+import { ReactionEntity } from "../entities/reaction_entity";
 import { NewInfoEntity } from "../entities/see_new_entity";
 
 export interface NewsDataSource {
   saveNews(news: NewsEntity): Promise<void>;
   getNewInfo(newId: number): Promise<NewInfoEntity>;
+  updateLikeDislike(reaction: ReactionEntity): Promise<ReactionEntity>;
 }
